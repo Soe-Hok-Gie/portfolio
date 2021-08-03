64eb691fa9f9b01d9ef30eb6eeff60bc552eb2d0
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-03 14:40:04 +0700

    solve problem section dengan spasi di height bukan tanda minus

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro";
-import Portfolio from "./components/portfolio/Portfolio";
-import Works from "./components/works/Works";
-import Testemonials from "./components/testemonials/Testemonials";
-import Contact from "./components/contact/Contact";
+import Topbar from "./components/topbar/Topbar"
+import Intro from "./components/intro/Intro"
+import Portfolio from "./components/portfolio/Portfolio"
+import Works from "./components/works/Works"
+import Testemonials from "./components/testemonials/Testemonials"
+import Contact from "./components/contact/Contact"
 import "./app.scss"
+
 //sections gak terbaca
 
 function App() {
   return (
     <div className="app">
       <Topbar/>
-        <div className="sections">
+      <div className="sections">
         <Intro/> 
         <Portfolio/> 
         <Works/> 
