826b9dbbbf82fc367c9478c0ee04cb435779c6d9
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-04 00:31:37 +0700

    menambahkan material-ui dengan icon person dan mail pada class itemContainer

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,5 @@
 import "./topbar.scss"
-
-
-
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
@@ -9,6 +7,14 @@ export default function Topbar() {
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Gembel.</a>
+                    <div className="itemContaner">
+                        <Person className="icon"/>
+                        <span>083336874</span>
+                    </div>
+                    <div className="itemContaner">
+                        <Mail className="icon "/>
+                        <span>@gembelelite.com</span>
+                    </div>
                 </div>
                 <div className="right">
                 </div>
