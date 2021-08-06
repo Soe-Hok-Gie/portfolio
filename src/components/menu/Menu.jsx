431b54fdf4617e7dcbb75c847cff3e903cb3434c
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-06 17:24:43 +0700

    membuat semua tag li active ketika di klik

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -10,16 +10,16 @@ export default function Menu({menuOpen, setMenuOpen}) {
                     <a href="#intro">Home</a>
                 </li>
 
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#portfolio">Portfolio</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#works">Works</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#testemonials">Testemonials</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
