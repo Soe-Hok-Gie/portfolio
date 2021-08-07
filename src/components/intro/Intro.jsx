b8568feca76a30810ac69a7ba66651a87fe0e10f
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-07 18:07:04 +0700

    membuat assets

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,23 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt=""/>
+                </div>
+            </div>
+            <div className="right ">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Gembele Elite</h1>
+                    <h3>Writer <span></span></h3>
+                </div>
+                <a href="portfolio">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+            </div>
         </div>
+        
     )
 }
+ 
