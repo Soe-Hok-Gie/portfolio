acae9f9c8e7911f8ef2e611574bdf40e5ea3c592
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-08 22:55:33 +0700

    membuat hook untuk textRef dgn useEffect

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,21 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
+
 
 export default function Intro() {
+
+    const textRef = useRef ();
+
+    useEffect (()=>{
+    init(textRef.current, { 
+        showCursor: false,
+        backDelay:  1500,
+        strings: ['Writer', 'React Newbie' ] 
+    });
+},[]);
+
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +27,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Gembele Elite</h1>
-                    <h3>Writer <span></span></h3>
+                    <h3>Freelance <span ref={textRef}></span></h3>
                 </div>
                 <a href="portfolio">
                     <img src="assets/down.png" alt=""/>
