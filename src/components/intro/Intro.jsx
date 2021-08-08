93ddb5a8c9e1c01536bddcf02d2ace5fe2359eee
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-08 21:50:30 +0700

    mengubah foto :D

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/man.png" alt=""/>
+                    <img src="assets/kakek.png" alt=""/>
                 </div>
             </div>
             <div className="right ">
