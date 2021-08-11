08946b36c065b4216fbfd49f510c65658c4f5cf6
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-08-11 17:52:24 +0700

    finish list feature portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,39 +8,39 @@ export default function Portfolio() {
                 <li className="active">Featured</li>
                 <li>Web Apps</li>
                 <li>Mobile Apps</li>
-                <li>Branding</li>
+                <li>Banking</li>
             </ul>
             <div className="container">
             <div className="item">
-                     <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/coding-1543907-1306295.png" alt=""/>
+                     <img src="https://image.freepik.com/free-vector/coding-programmer-workspace-with-cactus-coffee-book-vector-clip-art-illustration_138676-87.jpg" alt=""/>
                      
-                     <h3>Branding app</h3>
+                     <h3>Banking app</h3>
                 </div>
                 <div className="item">
-                     <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/coding-1543907-1306295.png" alt=""/>
+                     <img src="https://image.freepik.com/free-vector/coding-programmer-workspace-with-cactus-coffee-book-vector-clip-art-illustration_138676-87.jpg" alt=""/>
                      
-                     <h3>Branding app</h3>
+                     <h3>Banking app</h3>
                 </div>
                 <div className="item">
-                     <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/coding-1543907-1306295.png" alt=""/>
+                     <img src="https://image.freepik.com/free-vector/coding-programmer-workspace-with-cactus-coffee-book-vector-clip-art-illustration_138676-87.jpg" alt=""/>
                      
-                     <h3>Branding app</h3>
+                     <h3>Banking app</h3>
                 </div>
                 <div className="item">
-                     <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/coding-1543907-1306295.png" alt=""/>
+                     <img src="https://image.freepik.com/free-vector/coding-programmer-workspace-with-cactus-coffee-book-vector-clip-art-illustration_138676-87.jpg" alt=""/>
                      
-                     <h3>Branding app</h3>
+                     <h3>Banking app</h3>
                 </div>
             
                 <div className="item">
-                     <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/coding-1543907-1306295.png" alt=""/>
+                     <img src="https://image.freepik.com/free-vector/coding-programmer-workspace-with-cactus-coffee-book-vector-clip-art-illustration_138676-87.jpg" alt=""/>
                      
-                     <h3>Branding app</h3>
+                     <h3>Banking app</h3>
                 </div>
                 <div className="item">
-                     <img src="https://cdn.iconscout.com/icon/premium/png-128-thumb/coding-1543907-1306295.png" alt=""/>
+                     <img src="https://image.freepik.com/free-vector/coding-programmer-workspace-with-cactus-coffee-book-vector-clip-art-illustration_138676-87.jpg" alt=""/>
                      
-                     <h3>Branding app</h3>
+                     <h3>Banking app</h3>
                 </div>
             </div>
         </div>
